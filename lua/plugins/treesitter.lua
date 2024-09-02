5be1bba95a167069b04583a323b9ab979a04bf47
Edgar Lira <edgar.jj.29@gmail.com>
2024-09-02 17:51:37 -0300

    feat: adiciona styled-components no treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     run = ":TSUpdate",
     init = function()
         require("nvim-treesitter.configs").setup {
-            ensure_installed = { 'markdown', 'c', 'cpp', 'bash', 'lua', 'vim', 'python', 'c_sharp', 'git_config', 'gitignore', "vimdoc", "luadoc", "go", "typescript", "csv" },
+            ensure_installed = { 'markdown', 'c', 'cpp', 'bash', 'lua', 'vim', 'python', 'c_sharp', 'git_config', 'gitignore', "vimdoc", "luadoc", "go", "typescript", "csv", "css", "styled" },
             highlight = {
                 enable = true
             }
