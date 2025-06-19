488e15286ebb0bd84a04d7a49ae4a0db8cf398f9
Edgar Lira <edgar.jj.29@gmail.com>
2025-06-19 02:28:11 -0300

    feat: adiciona configurações avançadas nos plugins de indent e nvim_tree

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,14 +1,35 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
-    config = function() -- default configuration
+    config = function()
         require("ibl").setup({
+            enabled = true,
             scope = {
-                enabled = false,
+                enabled = true,
                 show_start = false,
                 show_end = false,
+                show_exact_scope = false,
+                injected_languages = false,
                 highlight = { "NonText" },
-                injected_languages = false
-            }
+                priority = 1024,
+                include = {
+                    node_type = {
+                        ["*"] = {
+                            "switch_statement",
+                            "match_statement",
+                            "case_statement",
+                            "case_clause",
+                            "match_expression",
+                            "function",
+                            "if_statement",
+                            "for_statement",
+                            "while_statement",
+                            "switch_statement",
+                            "with_statement",
+                            "try_statement"
+                        },
+                    }
+                },
+            },
         })
     end
 }
--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -3,10 +3,44 @@ return {
     version = "*",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        require("nvim-tree").setup {
+        local function my_on_attach(bufnr)
+            local function opts(desc)
+                return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+            end
+            local api = require("nvim-tree.api")
+            api.config.mappings.default_on_attach(bufnr)
+            vim.keymap.del('n', '<C-k>', { buffer = bufnr })
+            vim.keymap.set('n', 'K', api.node.show_info_popup, opts("Show info nvim-tree"))
+        end
+
+        require("nvim-tree").setup({
+            on_attach = my_on_attach,
             update_focused_file = {
-                enable = true,
+                enable = true
             },
-        }
+            view = {
+                centralize_selection = false,
+                cursorline = true,
+                debounce_delay = 15,
+                side = "left",
+                preserve_window_proportions = true,
+                number = false,
+                relativenumber = false,
+                signcolumn = "yes",
+                width = 30,
+            },
+            diagnostics = {
+                enable = true
+            },
+            renderer = {
+                indent_width = 2,
+                icons = {
+                    diagnostics_placement = "after"
+                },
+                indent_markers = {
+                    enable = true
+                }
+            }
+        })
     end
 }
