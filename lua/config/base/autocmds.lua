0e57157e0e2dd62909742ce8698ce0324dd4a984
EdgarLira25 <edgar.jj.29@gmail.com>
2025-06-22 22:07:52 +0000

    remove plugin de autosave para meu próprio autosave em lua

--- lua/config/base/autocmds.lua
+++ lua/config/base/autocmds.lua
@@ -1,1 +1,24 @@
 vim.cmd([[ autocmd FocusGained,BufEnter * checktime ]])
+
+local autosave_enabled = true
+local timer = vim.loop.new_timer()
+
+vim.api.nvim_create_autocmd({ "TextChanged", "InsertLeave" }, {
+    pattern = "*",
+    callback = function()
+        if not autosave_enabled then return end
+        timer:stop()
+        timer:start(1000, 0, vim.schedule_wrap(function()
+            local buf = vim.api.nvim_get_current_buf()
+            if vim.bo[buf].modifiable and vim.bo[buf].modified then
+                -- vim.notify("Save", vim.log.levels.INFO) -- para logs
+                vim.cmd("silent! write")
+            end
+        end))
+    end,
+})
+
+vim.api.nvim_create_user_command("ASToggle", function()
+    autosave_enabled = not autosave_enabled
+    print("AutoSave " .. (autosave_enabled and "enabled" or "disabled"))
+end, {})
